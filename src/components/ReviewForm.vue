9d591d6a4c6a68906c134907466d5a6ec7901a53
Worawut Aunkham <jameskater350@gmail.com>
2022-08-21 00:17:57 +0700

    Add style and design platform

--- src/components/ReviewForm.vue
+++ src/components/ReviewForm.vue
@@ -59,3 +59,82 @@ export default{
 }
 
 </script>
+
+<style scoped>
+body {
+    background-color: #f2f2f2;
+    margin: 0px;
+    font-family: tahoma;
+    color: #282828;
+}
+.review-form {
+    display: flex;
+    flex-direction: column;
+    width: 425px;
+    padding: 20px;
+    margin: 40px;
+    border: 2px solid #d8d8d8;
+    background-color: white;
+    -webkit-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
+    -moz-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
+    box-shadow: 2px 15px -12px rgba(0, 0, 0, 0.57);
+}
+
+.review-container {
+    width: 425px;
+    padding: 20px;
+    background-color: white;
+    -webkit-box-shadow: 0px 2px 20px -12px rgba(0, 0, 0, 0.57);
+    -moz-box-shadow: 0px 2px 20px -12px rgba(0, 0, 0, 0.57);
+    box-shadow: 2px 20px -12px rgba(0, 0, 0, 0.57);
+    margin-left: 40px;
+    border: 2px solid #d8d8d8;
+}
+
+.review-container li {
+    margin-bottom: 30px;
+}
+
+.review-form .button {
+    display: block;
+    margin: 30px auto;
+}
+
+select {
+    height: 40px;
+    font-size: 20px;
+    background-color: white;
+    cursor: pointer;
+}
+
+textarea {
+    width: 95%;
+    height: 70px;
+    padding: 10px;
+    font-size: 20px;
+    margin-bottom: 20px;
+}
+
+ul {
+    list-style-type: none;
+}
+
+.button {
+    margin: 30px;
+    background-color: #39495c;
+    border-radius: 5px;
+    font-size: 18px;
+    width: 160px;
+    height: 60px;
+    color: white;
+    padding: 20px;
+    box-shadow: inset 0 -0.6em 1em -0.35em rgba(0, 0, 0, 0.17), inset 0 0.6em 2em -0.3em rgba(255, 255, 255, 0.15), inset 0 0 0em 0.05em rgba(255, 255, 255, 0.12);
+    text-align: center;
+    cursor: pointer;
+}
+
+.disabledButton {
+    background-color: #d8d8d8;
+    cursor: not-allowed;
+}
+</style>
