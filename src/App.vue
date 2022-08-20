bb7be35d7d066632a885276ae011dc9f39fd492c
Wongsathorn632115035 <wongsathorn_w@cmu.ac.th>
2022-08-20 22:08:45 +0700

    Update sub topic

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@
     {{ GStore.flashMessage }}
   </div>
   <div id="nav">
-    <router-link :to="{ name: 'EventList' }">Home</router-link> |
-    <router-link :to="{ name: 'About' }">About</router-link>
+    <router-link :to="{ name: 'EventList' }">Home</router-link>
   </div>
 
   <!-- new element -->
